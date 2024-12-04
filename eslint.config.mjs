0427dd36e88133f29edb5c62940272cd3235925a
Binod Gautam <gautambinod629@gmail.com>
2024-12-04 22:01:26 +0545

    setup git hook husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,21 +4,21 @@ import tseslint from '@typescript-eslint/eslint-plugin';
 import tsParser from '@typescript-eslint/parser';
 
 export default [
-    {
-        files: ['**/*.ts', '**/*.tsx'],
-        languageOptions: {
-            parser: tsParser,
-            parserOptions: {
-                tsconfigRootDir: import.meta.dirname,
-                project: './tsconfig.json',
-            },
-        },
-        plugins: {
-            '@typescript-eslint': tseslint,
-        },
-        rules: {
-            'no-console': 'error',
-        },
-        ignores: ['node_modules', 'dist'],
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: './tsconfig.json',
+      },
     },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-console': 'error',
+    },
+    ignores: ['node_modules', 'dist'],
+  },
 ];
