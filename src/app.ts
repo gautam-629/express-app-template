aaf0074fd4ba3511bd7672fecc851f5a50f69374
Binod Gautam <gautambinod629@gmail.com>
2024-12-05 21:36:37 +0545

    add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,31 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import createHttpError, { HttpError } from 'http-errors';
+import logger from './config/logger';
 
 let app = express();
 
-app.use('/', (req, res) => {
+app.use('/', (req, res, next) => {
+  // const err=createHttpError(401,'Unauthorized')
+  next(createHttpError(401, 'Unauthorized'));
+  return;
+  // throw err;
+
   res.send('Welcome to the Express app');
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+  logger.error(err.message);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode).json({
+    error: [
+      {
+        type: err.name,
+        name: err.name,
+        path: '',
+        location: '',
+      },
+    ],
+  });
 });
 
 export default app;
